6e65991524889a886b28ea906bda1d4c296376fb
Vinicius Andrade <58332861+dev-vinicius-andrade@users.noreply.github.com>
2023-03-17 19:33:16 -0300

    adding new FillWith object extension and modifing README

---
+++ src/Tests/Codescovery.Library.Commons.Test/Tests/ObjectExtensionsTester.cs
@@ -0,0 +1,92 @@
+﻿using Codescovery.Library.Commons.Test.Enums;
+using System.Diagnostics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Codescovery.Library.Commons.Builders;
+using Codescovery.Library.Commons.Extensions;
+using Codescovery.Library.Commons.Test.Entities;
+
+namespace Codescovery.Library.Commons.Test.Tests
+{
+
+    [TestClass]
+    public class ObjectExtensionsTester
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+#if DEBUG
+            Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+#endif
+        }
+        
+        [TestMethod]
+        public void CopyPropertiesFromObjectToAnotherWithModifiedValueNotNull()
+        {
+            var randomizer = DeepRandomizerServiceBuilder.BuildDefault();
+            var sourceObject = randomizer.Randomize<ClonableExampleClass>(4);
+            var newObject = new ClonableExampleClass();
+            newObject.FillWith(sourceObject);
+            newObject.ExampleString = "New string value";
+            var newObjectJson = JsonSerializer.Serialize(newObject);
+            var sourceObjectJson = JsonSerializer.Serialize(sourceObject);
+
+            Assert.AreNotEqual(newObjectJson, sourceObjectJson);
+        }
+        [TestMethod]
+        public void CopyPropertiesFromObjectToAnotherWithModifiedValueNull()
+        {
+            ClonableExampleClass? sourceObject = null;
+            var newObject = new ClonableExampleClass();
+            newObject.FillWith(sourceObject);
+            newObject.ExampleString = "New string value";
+            var newObjectJson = JsonSerializer.Serialize(newObject);
+            var sourceObjectJson = JsonSerializer.Serialize(sourceObject);
+
+            Assert.AreNotEqual(newObjectJson, sourceObjectJson);
+        }
+
+        [TestMethod]
+        public void CopyPropertiesFromObjectToAnotherWithoutModifiedValueNotNull()
+        {
+            var randomizer = DeepRandomizerServiceBuilder.BuildDefault();
+            var sourceObject = randomizer.Randomize<ClonableExampleClass>(4);
+            var newObject = new ClonableExampleClass();
+            newObject.FillWith(sourceObject);
+            var newObjectJson = JsonSerializer.Serialize(newObject);
+            var sourceObjectJson = JsonSerializer.Serialize(sourceObject);
+
+            Assert.AreEqual(newObjectJson, sourceObjectJson);
+        }
+        [TestMethod]
+        public void CopyPropertiesFromObjectToAnotherWithoutModifiedValueNull()
+        {
+            ClonableExampleClass? sourceObject = null;
+            var newObject = new ClonableExampleClass();
+            newObject.FillWith(sourceObject);
+            var newObjectJson = JsonSerializer.Serialize(newObject);
+            var sourceObjectJson = JsonSerializer.Serialize(sourceObject);
+
+            Assert.AreNotEqual(newObjectJson, sourceObjectJson);
+        }
+        [TestMethod]
+        public void CopyPropertiesFromObjectToAnotherWithoutModifiedValueNewObjectNull()
+        {
+            ClonableExampleClass? sourceObject = null;
+            ClonableExampleClass? newObject = null;
+            newObject.FillWith(sourceObject);
+            var newObjectJson = JsonSerializer.Serialize(newObject);
+            var sourceObjectJson = JsonSerializer.Serialize(sourceObject);
+
+            Assert.AreEqual(newObjectJson, sourceObjectJson);
+        }
+        [TestMethod]
+        public void CopyPropertiesFromObjectToAnotherWithoutModifiedValueNewObjectNullSourceObjectNotNull()
+        {
+            var randomizer = DeepRandomizerServiceBuilder.BuildDefault();
+            var sourceObject = randomizer.Randomize<ClonableExampleClass>(4);
+            ClonableExampleClass? newObject = null;
+            Assert.ThrowsException<ArgumentNullException>(()=>newObject.FillWith(sourceObject));
+        }
+    }
+}
